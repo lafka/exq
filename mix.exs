1982f68e11708270793e072adca031857bcfc98f
Olav Frengstad <olav@fwt.no>
2015-06-21 16:51:33 +0200

    Publish to hex.pm

--- mix.exs
+++ mix.exs
@@ -2,12 +2,16 @@ defmodule Exq.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :exq,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :exq,
+      description: "Library for generating functions to match collection of maps",
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      package: package
+    ]
   end
 
   def application do
@@ -16,5 +20,15 @@ defmodule Exq.Mixfile do
 
   defp deps do
     []
+  end
+
+  defp package do
+    [
+      licenses: "BSD",
+      links: %{
+        "GitHub" => "https://github.com/lafka/exq"
+      },
+      contributors: ["Olav Frengstad"]
+    ]
   end
 end
